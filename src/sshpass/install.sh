1651ed9849033b7b9eb6ef36889d58a0c80fcf1f
Hans Spaans <hspaans@gmail.com>
2023-12-13 17:08:43 +0100

    Add sshpass feature and test scripts
    * Add sshpass feature and test scripts
    
    * Merge branch 'master' into hspaans/sshpass/init

---
+++ src/sshpass/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+# Install dependencies if missing
+check_packages sshpass
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
